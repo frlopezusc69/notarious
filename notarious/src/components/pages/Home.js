c8c3fd14a2f1ba396582e6d32717d603cfceb4a0
Felix Lopez <81645633+frlopezusc69@users.noreply.github.com>
2021-06-16 00:51:52 -0700

    first commit

---
+++ notarious/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import '../../App.css';
+import Mainpart from '../Mainpart';
+
+function Home () {
+    return (
+        <>
+            <Mainpart />
+        </>
+    )
+}
+
+export default Home;
